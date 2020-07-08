d7a0838264f74950cf50918e7775765efd650308
llKyo <k.trivinod@gmail.com>
2020-07-07 22:20:03 -0400

    Fix autenticación de obra
    Se eliminó el loop generado con el script de comprobación de la vista
    administrador obra

--- js/obras.js
+++ js/obras.js
@@ -6,17 +6,13 @@ firebase.auth().onAuthStateChanged(function(user) {
         // User is not signed in.
         location.href = 'index.php';
         alert("no está autenticado");
-
-
     } else {
         // User is  signed in.
         db.collection("clientes").onSnapshot((querySnapshot) => {
             querySnapshot.forEach((doc) => {
                 if (doc.data().correo == user.email) {
                     location.href = 'clientes.php';
-                } else {
-                    location.href = 'obras.php'; //Remplazr href
-                }
+                } 
             })
 
         })
